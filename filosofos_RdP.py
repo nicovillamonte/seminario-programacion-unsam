fb126706c998f07f5debbf01dad54785ff28ac98
nicovillamonte <nicovillamonte@gmail.com>
2023-10-09 19:35:28 -0300

    Terminacion y mejora de los filosofos comenzales con la red de petri

--- filosofos_RdP.py
+++ filosofos_RdP.py
@@ -1,83 +1,112 @@
-#! EN PROCESO
-
-from snakes.nets import *
+from snakes.nets import PetriNet, Place, Transition, dot, Value
+from threading import Thread, Lock
+from colorama import Fore, Back, Style
+import os
 import time
+import random
 
-# Creamos la red
-net = PetriNet('Red')
+output_lock = Lock()
 
-# Pedimos al usuario que ingrese la cantidad de filósofos
-N = int(input("Cantidad de filosofos: "))
-
-# Funcion para crear los filósofos
-def createPhilosopher(index):
-  p0 = Place(f"pensando{index}", [dot])
-  p1 = Place(f"comiendo{index}", [])
-  return [p0,p1]
-
-
-# Creación de los filósofos
-filosofos = [createPhilosopher(i) for i in range(N)]
-# Creación de los cubiertos
-cubiertos = [Place(f"cubierto{i}", [dot]) for i in range(N)]
-
-# Creación de las transiciones
-dejar_cubiertos = [Transition(f'tomar_{i}') for i in range(N)]
-tomar_cubiertos = [Transition(f'dejar_{i}') for i in range(N)]
-
-# Agregado a la red de los cubiertos
-for cubierto in cubiertos:
-  net.add_place(cubierto)
-
-# Agregado a la red de los estados de los filosofos
-for filosofo in filosofos:
-  for estado in filosofo:
-    net.add_place(estado)
-
-# Agregado a la red de las transiciones
-for grupo_transiciones in [dejar_cubiertos, tomar_cubiertos]:
-  for transicion in grupo_transiciones:
-    net.add_transition(transicion)
-
-for index, filosofo in enumerate(filosofos):
-  index_anterior = (index - 1) if index - 1 >= 0 else N - 1
-  
-  # Agregar inputs y outputs de la tansicion de tomar cubiertos del filósofo
-  net.add_input(str(cubiertos[index]), str(tomar_cubiertos[index]), Value(dot))
-  net.add_input(str(cubiertos[index_anterior]), str(tomar_cubiertos[index]), Value(dot))
-  net.add_input(str(filosofo[0]), str(tomar_cubiertos[index]), Value(dot))
-  net.add_output(str(filosofo[1]), str(tomar_cubiertos[index]), Value(dot))
-  
-  # Agregar inputs y outputs de la tansicion de dejar cubiertos del filósofo
-  net.add_input(str(filosofo[1]), str(dejar_cubiertos[index]), Value(dot))
-  net.add_output(str(cubiertos[index]), str(dejar_cubiertos[index]), Value(dot))
-  net.add_output(str(cubiertos[index_anterior]), str(dejar_cubiertos[index]), Value(dot))
-  net.add_output(str(filosofo[0]), str(dejar_cubiertos[index]), Value(dot))
-  
-# Imprimir en tiempo real una simulación de las transiciones
-for i in range(N):
-    trans = str(tomar_cubiertos[i])
-    modes = net.transition(trans).modes()
-    if trans in [t.name for t in net.transition()]:
-        if modes:
-            net.transition(trans).fire(modes[0])
-
+def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    # Mostrar el estado de los cubiertos
-    for i in range(N):
-        print(f"Fork{i}", net.place(str(cubiertos[i])).tokens)
+def print_c(text, color = Fore.RESET, bg_color = Back.RESET):
+    print(bg_color + color + text + Style.RESET_ALL)
 
-    # Mostrar el estado de los filósofos
-    for i, (pensando, comiendo) in enumerate(filosofos):
-        estado = "comiendo" if net.place(str(comiendo)).tokens else "pensando"
-        print(f"Filósofo{i} está {estado}")
+class PhilosopherState:
+    THINKING = "pensando"
+    EATING = "comiendo"
 
+class Philosopher(Thread):
+    def __init__(self, id, petri_net, initial_state=PhilosopherState.THINKING):
+        super().__init__()
+        self.id = id
+        self.initial_state = initial_state
+        self.petri_net = petri_net
+        self.eats = 0
+        self.create_places_and_transitions()
+
+    def create_places_and_transitions(self):
+        self.thinking_place = self.create_place(PhilosopherState.THINKING)
+        self.eating_place = self.create_place(PhilosopherState.EATING)
+        self.take_forks_trans = self.create_transition(f"take_forks_{self.id}")
+        self.put_forks_trans = self.create_transition(f"put_forks_{self.id}")
+
+    def create_place(self, state):
+        place_name = f"{state}_{self.id}"
+        initial_token = [dot] if self.initial_state == state else []
+        place = Place(place_name, initial_token)
+        self.petri_net.add_place(place)
+        return place
+
+    def create_transition(self, name):
+        transition = Transition(name)
+        self.petri_net.add_transition(transition)
+        return transition
+
+    def run(self):
+        self.eats = 0
+        while self.eats < 3:
+            self.execute_transition(self.take_forks_trans)
+            self.show_status()
+            self.execute_transition(self.put_forks_trans, eats = True)
+            self.show_status()
+
+    def execute_transition(self, transition, eats = False):
+        modes = transition.modes()
+        if modes:
+            transition.fire(modes[0])
+            if eats:
+                self.eats += 1
+
+    def show_status(self):
+        print_status(self.petri_net, philosophers, forks)
+        time.sleep(random.uniform(1, 5))
+
+def print_status(petri_net, philosophers, forks):
+    with output_lock:
+        clear_screen()
+        
+        print_c("Cubiertos:")
+        for i, fork in enumerate(forks):
+            print_c(f"Cubierto {i+1} " + str(fork.tokens), color=Fore.GREEN if fork.tokens else Fore.RED)
+
+        print_c("\nFilosofos:")
+        for i, philosopher in enumerate(philosophers):
+            state = "satisfecho" if philosopher.eats >= 3 else petri_net.place(f"{PhilosopherState.EATING}_{i}").tokens and PhilosopherState.EATING or PhilosopherState.THINKING
+            print_c(f"Filósofo {i+1} está {state}", color=Fore.GREEN if state == "satisfecho" else (Fore.CYAN if state == PhilosopherState.THINKING else Fore.YELLOW))
+
+def create_fork(id, petri_net):
+    place = Place(f"cubierto_{id}", [dot])
+    petri_net.add_place(place)
+    return place
+
+if __name__ == "__main__":
+    N = int(input("Cantidad de filosofos: "))
+    petri_net = PetriNet('Red')
+    philosophers = [Philosopher(i, petri_net) for i in range(N)]
+    forks = [create_fork(i, petri_net) for i in range(N)]
+
+    for i, philosopher in enumerate(philosophers):
+        prev = (i - 1) if i - 1 >= 0 else N - 1
+        petri_net.add_input(forks[i].name, philosopher.take_forks_trans.name, Value(dot))
+        petri_net.add_input(forks[prev].name, philosopher.take_forks_trans.name, Value(dot))
+        petri_net.add_input(philosopher.thinking_place.name, philosopher.take_forks_trans.name, Value(dot))
+        petri_net.add_output(philosopher.eating_place.name, philosopher.take_forks_trans.name, Value(dot))
+
+        petri_net.add_input(philosopher.eating_place.name, philosopher.put_forks_trans.name, Value(dot))
+        petri_net.add_output(forks[i].name, philosopher.put_forks_trans.name, Value(dot))
+        petri_net.add_output(forks[prev].name, philosopher.put_forks_trans.name, Value(dot))
+        petri_net.add_output(philosopher.thinking_place.name, philosopher.put_forks_trans.name, Value(dot))
+    
+    print_status(petri_net, philosophers, forks)
     time.sleep(2)
 
+    for philosopher in philosophers:
+        philosopher.start()
 
-# Print forks
-os.system('cls' if os.name == 'nt' else 'clear')    # Limpiar la pantalla
-for i in range(N):
-  print(f"Fork{i}", net.place(str(cubiertos[i])).tokens)
-  
+    for philosopher in philosophers:
+        philosopher.join()
+
+    print_status(petri_net, philosophers, forks)
+    print_c("Todos los filósofos están satisfechos", color=Fore.RED, bg_color=Back.YELLOW)
